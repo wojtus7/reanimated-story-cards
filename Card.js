aea4e5e3c8e7438c17bbb9198fec71cc3f98ff7c
Wojciech Stanisz <wojciech.stanisz@swmansion.com>
2021-03-10 15:56:56 +0100

    Add Text on Card

--- Card.js
+++ Card.js
@@ -7,7 +7,7 @@ import Animated, {
   interpolate,
   Extrapolate,
 } from 'react-native-reanimated';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, Text} from 'react-native';
 import {PanGestureHandler} from 'react-native-gesture-handler';
 
 const Card = ({
@@ -59,12 +59,52 @@ const Card = ({
     };
   });
 
+  const animatedRightTextWrapper = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(x.value, [15, 70], [0, 1], Extrapolate.CLAMP),
+      transform: [
+        {
+          rotateZ: interpolate(
+            x.value,
+            [0, 50],
+            [0, -0.03],
+            Extrapolate.EXTEND,
+          ),
+        },
+      ],
+    };
+  });
+
+  const animatedLeftTextWrapper = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(x.value, [-15, -70], [0, 1], Extrapolate.CLAMP),
+      transform: [
+        {
+          rotateZ: interpolate(
+            x.value,
+            [-50, 0],
+            [0.03, 0],
+            Extrapolate.EXTEND,
+          ),
+        },
+      ],
+    };
+  });
+
   return (
     <View style={styles.cardWrapper}>
       <PanGestureHandler onGestureEvent={gestureHandler}>
         <Animated.View
-          style={[animatedMovableCard, styles.wrapper, {backgroundColor}]}
-        />
+          style={[animatedMovableCard, styles.wrapper, {backgroundColor}]}>
+          <Animated.View
+            style={[animatedRightTextWrapper, styles.topTextWrapper]}>
+            <Text style={styles.topText}>{rightText}</Text>
+          </Animated.View>
+          <Animated.View
+            style={[animatedLeftTextWrapper, styles.topTextWrapper]}>
+            <Text style={[styles.topText, styles.textLeft]}>{leftText}</Text>
+          </Animated.View>
+        </Animated.View>
       </PanGestureHandler>
     </View>
   );
@@ -79,6 +119,23 @@ const styles = StyleSheet.create({
   },
   cardWrapper: {
     height: 240,
+  },
+  topTextWrapper: {
+    position: 'absolute',
+    width: '120%',
+    left: '-10%',
+    top: '-10%',
+    paddingTop: '15%',
+    paddingHorizontal: '15%',
+    backgroundColor: 'rgba(0,0,0,0.4)',
+    padding: 15,
+    zIndex: 10,
+  },
+  topText: {
+    color: '#fff',
+  },
+  textLeft: {
+    textAlign: 'right',
   },
 });
 
