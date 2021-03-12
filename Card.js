b7177304803e84beb581744327df9aea1f139d8c
Wojciech Stanisz <wojciech.stanisz@swmansion.com>
2021-03-12 14:45:49 +0100

    make project bare

--- Card.js
+++ Card.js
@@ -1,14 +1,5 @@
 import React from 'react';
-import Animated, {
-  useSharedValue,
-  useAnimatedStyle,
-  useAnimatedGestureHandler,
-  withSpring,
-  interpolate,
-  Extrapolate,
-} from 'react-native-reanimated';
 import {StyleSheet, View} from 'react-native';
-import {PanGestureHandler} from 'react-native-gesture-handler';
 
 const Card = ({
   onChooseLeftAnswer,
@@ -18,54 +9,9 @@ const Card = ({
   image,
   backgroundColor,
 }) => {
-  const x = useSharedValue(0);
-  const y = useSharedValue(0);
-
-  const cardSpringConfig = {
-    damping: 100,
-    stiffness: 90,
-    mass: 0.5,
-  };
-
-  const gestureHandler = useAnimatedGestureHandler({
-    onStart: (_, ctx) => {
-      ctx.startX = x.value;
-      ctx.startY = y.value;
-    },
-    onActive: (event, ctx) => {
-      x.value = ctx.startX + event.translationX;
-      y.value = ctx.startY + event.translationY;
-    },
-    onEnd: () => {
-      x.value = withSpring(0, cardSpringConfig);
-      y.value = withSpring(0, cardSpringConfig);
-    },
-  });
-
-  const animatedMovableCard = useAnimatedStyle(() => {
-    return {
-      transform: [
-        {
-          rotateZ: interpolate(
-            x.value,
-            [-50, 50],
-            [-0.05, 0.05],
-            Extrapolate.EXTEND,
-          ),
-        },
-        {translateX: x.value},
-        {translateY: y.value},
-      ],
-    };
-  });
-
   return (
     <View style={styles.cardWrapper}>
-      <PanGestureHandler onGestureEvent={gestureHandler}>
-        <Animated.View
-          style={[animatedMovableCard, styles.wrapper, {backgroundColor}]}
-        />
-      </PanGestureHandler>
+      <View style={[styles.wrapper, {backgroundColor}]} />
     </View>
   );
 };
--- Screen.js
+++ Screen.js
@@ -1,84 +1,17 @@
 import {View, StyleSheet} from 'react-native';
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import Card from './Card';
-import PlaceholderBackCards from './PlaceholderBackCards';
-import Question from './Question';
-import PowerIndicators from './PowerIndicators';
-import PlaceholderBackStaticCard from './PlaceholderBackStaticCard';
-import StartButton from './StartButton';
 import useGeneratedCards from './useGeneratedCards';
 
 export default function AnimatedStyleUpdateExample() {
   const {getCardByIndex} = useGeneratedCards();
   const [currentCard, setCurrentCard] = useState(getCardByIndex(0));
-  const [currentCardIndex, setCurrentCardIndex] = useState(0);
-  const [currentMood, setCurrentMood] = useState({happy: [], sad: []});
-
-  const [showStartButton, setShowStartButton] = useState(true);
-  const [showAnimatedReverseCard, setShowAnimatedReverseCard] = useState(false);
-  const [showReverseCard, setShowReverseCard] = useState(false);
-  const [showCard, setShowCard] = useState(false);
-  const [showQuestion, setShowQuestion] = useState(false);
-
-  // TODO refactor those settimeouts
-  const showNextCard = (timeout) => {
-    setTimeout(() => {
-      setShowCard(true);
-      setTimeout(() => {
-        setShowQuestion(true);
-      }, 100);
-    }, timeout);
-  };
-
-  const onStartGame = () => {
-    setCurrentCard(getCardByIndex(currentCardIndex));
-    setCurrentCardIndex(currentCardIndex + 1);
-
-    setTimeout(() => {
-      setShowStartButton(false);
-      setShowAnimatedReverseCard(true);
-    }, 500);
-    setTimeout(() => {
-      setShowReverseCard(true);
-      setTimeout(() => {
-        setShowAnimatedReverseCard(false);
-      }, 100);
-    }, 2000);
-    showNextCard(2500);
-  };
-
-  const onChooseLeftAnswer = () => {
-    setCurrentMood(currentCard.onLeft);
-    createNewCard();
-    setTimeout(() => {
-      setCurrentMood({happy: [], sad: []});
-    }, 200);
-  };
-
-  const onChooseRightAnswer = () => {
-    setCurrentMood(currentCard.onRight);
-    createNewCard();
-    setTimeout(() => {
-      setCurrentMood({happy: [], sad: []});
-    }, 200);
-  };
-
-  const createNewCard = () => {
-    setShowQuestion(false);
-    setTimeout(() => {
-      // let it fly away in peace for 300 ms
-      setCurrentCard(getCardByIndex(currentCardIndex));
-      setCurrentCardIndex(currentCardIndex + 1);
-      setShowCard(false);
-    }, 300);
-    showNextCard(700);
-  };
 
   return (
     <View style={styles.wrapper}>
       <Card
-        onChooseLeftAnswer={onChooseLeftAnswer}
-        onChooseRightAnswer={onChooseRightAnswer}
+        onChooseLeftAnswer={() => null}
+        onChooseRightAnswer={() => null}
         leftText={currentCard.leftText}
         rightText={currentCard.rightText}
         image={currentCard.image}
@@ -94,25 +27,5 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  cardWrapper: {
-    height: 240,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  questionWrapper: {
-    height: 100,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  nameWrapper: {
-    height: 100,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  topWrapper: {
-    width: '100%',
-    height: 200,
-    backgroundColor: '#ccc',
   },
 });
