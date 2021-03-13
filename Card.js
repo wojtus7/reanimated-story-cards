a22c8157ae26dae94073b1543056039d61feb364
Wojciech Stanisz <wojciech.stanisz@swmansion.com>
2021-03-13 02:01:25 +0100

    Fix rotate on android

--- Card.js
+++ Card.js
@@ -50,16 +50,16 @@ const Card = ({
   const animatedMovableCard = useAnimatedStyle(() => {
     return {
       transform: [
-        {translateX: x.value},
-        {translateY: y.value},
         {
-          rotateZ: interpolate(
+          rotateZ: `${interpolate(
             x.value,
             [-50, 50],
             [-0.05, 0.05],
             Extrapolate.EXTEND,
-          ),
+          )}rad`,
         },
+        {translateX: x.value},
+        {translateY: y.value},
       ],
     };
   });
@@ -95,12 +95,12 @@ const Card = ({
       opacity: interpolate(x.value, [15, 70], [0, 1], Extrapolate.CLAMP),
       transform: [
         {
-          rotateZ: interpolate(
+          rotateZ: `${interpolate(
             x.value,
             [0, 50],
             [0, -0.03],
             Extrapolate.EXTEND,
-          ),
+          )}rad`,
         },
       ],
     };
@@ -111,12 +111,12 @@ const Card = ({
       opacity: interpolate(x.value, [-15, -70], [0, 1], Extrapolate.CLAMP),
       transform: [
         {
-          rotateZ: interpolate(
+          rotateZ: `${interpolate(
             x.value,
             [-50, 0],
             [0.03, 0],
             Extrapolate.EXTEND,
-          ),
+          )}rad`,
         },
       ],
     };
